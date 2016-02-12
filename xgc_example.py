dc3163958d3144398ec13f60dc8136afe3c6c270
Randy Michael Churchill <rmc256@gmail.com>
2016-02-12 13:36:06 -0500

    Added example file

---
+++ xgc_example.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+import numpy as np
+import xgc
+from matplotlib.tri import Triangulation, LinearTriInterpolator
+import matplotlib.pyplot as plt
+
+#limit data to the [Rmin,Rmax,Zmin,Zmax] box, and read only the first two toroidal planes
+Rmin=2.2
+Rmax=2.31
+Zmin=-0.25
+Zmax=0.4
+phi_start=0
+phi_end=1
+
+fileDir='/global/project/projectdirs/m499/jlang/particle_pinch/'
+
+#load XGC data, and calculate normalized electron density
+loader=xgc.load(fileDir,Rmin=Rmin,Rmax=Rmax,Zmin=Zmin,Zmax=Zmax,phi_start=phi_start,phi_end=phi_end)
+
+#plot the poloidal mesh
+plt.figure(1)
+plt.triplot(loader.RZ[:,0],loader.RZ[:,1],loader.tri)
+
+
+#calculate the totoal electron density, and normalize to 
+n_e=loader.calcNeTotal()
+neNorm=np.einsum('i...,i...->i...',n_e,1./n_e[:,0,:])
+
+#setup mesh grid
+Ri=np.linspace (loader.Rmin,loader.Rmax,400)
+Zi=np.linspace (loader.Zmin,loader.Zmax,400)
+RI,ZI=np.meshgrid(Ri,Zi)
+
+#interpolate using the TriInterpolator class. Should be what tricontourf() uses
+triObj=Triangulation(loader.RZ[:,0],loader.RZ[:,1],loader.tri)
+tci=LinearTriInterpolator(triObj,neNorm[:,80])
+out=tci(RI,ZI)
+
+plt.figure(2)
+plt.contourf(RI,ZI,out,100)
