0ba33eef40448110840d8a75ded34adb953b1fa7
Michael Churchill <rmc256@gmail.com>
2016-02-12 10:55:21 -0800

    Added more info in xgc_example

--- xgc_example.py
+++ xgc_example.py
@@ -22,9 +22,11 @@ plt.figure(1)
 plt.triplot(loader.RZ[:,0],loader.RZ[:,1],loader.tri)
 
 
-#calculate the totoal electron density, and normalize to 
+#calculate the totoal electron density, and normalize to first time frame
+#n_e will be size [Nverts,Nplanes,Ntimes], where Nverts is the number of 
+#poloidal plane unstructured mesh vertices, Nplanes the number of toroidal planes, and Ntimes the number of time slices
 n_e=loader.calcNeTotal()
-neNorm=np.einsum('i...,i...->i...',n_e,1./n_e[:,0,:])
+neNorm=np.einsum('i...j,i...->i...j',n_e,1./n_e[:,:,0])
 
 #setup mesh grid
 Ri=np.linspace (loader.Rmin,loader.Rmax,400)
@@ -33,8 +35,16 @@ RI,ZI=np.meshgrid(Ri,Zi)
 
 #interpolate using the TriInterpolator class. Should be what tricontourf() uses
 triObj=Triangulation(loader.RZ[:,0],loader.RZ[:,1],loader.tri)
-tci=LinearTriInterpolator(triObj,neNorm[:,80])
+tci=LinearTriInterpolator(triObj,neNorm[:,0,80]) #interpolate for the index 0 toroidal plane, index 80 time slice
 out=tci(RI,ZI)
+
+sepInds = np.where(np.abs(loader.psin-1.0)<1e-4)[0]
 
 plt.figure(2)
 plt.contourf(RI,ZI,out,100)
+plt.clim([0,3])
+plt.plot(loader.RZ[sepInds,0],loader.RZ[sepInds,1],'w--')
+plt.colorbar()
+plt.xlabel('R [m]')
+plt.ylabel('Z [m]')
+plt.show()
